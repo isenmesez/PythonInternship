241c4f119980579f5a709a3c17f45698e5137fb5
Diliara <75849658+isenmesez@users.noreply.github.com>
2021-07-12 07:59:34 +0300

    Add files via upload

---
+++ 5.1b, 5.1d.py
@@ -0,0 +1,29 @@
+print('5.1b, 5.1d')
+london_co = {
+    "r1": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "4451",
+        "ios": "15.4",
+        "ip": "10.255.0.1"
+    },
+    "r2": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "4451",
+        "ios": "15.4",
+        "ip": "10.255.0.2"
+    },
+    "sw1": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "3850",
+        "ios": "3.6.XE",
+        "ip": "10.255.0.101",
+        "vlans": "10,20,30",
+        "routing": True
+    }
+}
+n = input('Введите имя устройства: (r1,r2 or sw1): ')
+p = str.lower(input('Введите имя параметра (' + ", ".join(london_co[n].keys()) + '): '))
+print(london_co[n][p])
