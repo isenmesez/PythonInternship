53112617a01e40bc22206c5c611070c46cfc557b
Diliara <75849658+isenmesez@users.noreply.github.com>
2021-07-14 10:47:15 +0300

    Add files via upload

---
+++ 6.2a.py
@@ -0,0 +1,20 @@
+print(6.2)
+ip = input('Введите ip адрес:')
+ip1 = ip.split('.')
+n = ip.find('.',0,len(ip))
+if n == 4:
+    if int(ip1[0]) in range(0,255) and int(ip1[1]) in range(0,255) and int(ip1[2]) in range(0,255) and int(ip1[3]) in range(0,255):
+        if 1 <= int(ip1[0]) <= 223:
+            print('unicast')
+        elif 239 <= int(ip1[0]) <= 244:
+            print('multicast')
+        elif ip == '255.255.255.255':
+            print('local broadcast')
+        elif ip == '0.0.0.0':
+            print('unassigned')
+        else:
+            print('unused')
+    else:
+        print('Неправильный IP-адрес')
+else:
+    print('Неправильный IP-адрес')
